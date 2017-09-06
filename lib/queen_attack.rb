dd0e883b0cd0510a910c2f458bb7419f5fdc346b
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-05 17:42:48 -0700

    pass all test

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
 def queen_attack?(queen_corrdinates, piece_corrdinates)
-  queen_corrdinates.first == piece_corrdinates.first || queen_corrdinates.last == piece_corrdinates.last
+  queen_corrdinates.first == piece_corrdinates.first || queen_corrdinates.last == piece_corrdinates.last ||
+  (queen_corrdinates.first - piece_corrdinates.first).abs == (queen_corrdinates.last - piece_corrdinates.last).abs
 end
